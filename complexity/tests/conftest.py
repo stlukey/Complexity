86fb011cffc4288124b8c4b19a869cbfdc6dd8d1
Luke Southam <luke@devthe.com>
2015-01-25 04:31:47 +0000

    Improved test fixtures.

--- complexity/tests/conftest.py
+++ complexity/tests/conftest.py
@@ -7,18 +7,21 @@
     :copyright: (c) 2015 Luke Southam <luke@devthe.com>.
     :license: New BSD, see LICENSE for more details.
 """
+import os
 import pytest
 
 from complexity import create_app
 
 @pytest.fixture
-def app(tmpfile):
-    return create_app(SHELVE_FILENAME=tmpfile)
+def app(request, tmpdir):
+    shelve = str(tmpdir.join('test_complexity.bin'))
+    def clean_up():
+        os.unlink(shelve)
+        tmpdir.remove()
+    request.addfinalizer(clean_up)
+    return create_app(SHELVE_FILENAME=shelve)
 
 @pytest.fixture
-def client(app):
+def test_client(app):
     return app.test_client()
-
-def test_foo(client):
-    print client.get('/')
 
--- complexity/tests/test_quiz.py
+++ complexity/tests/test_quiz.py
@@ -8,3 +8,5 @@
     :license: New BSD, see LICENSE for more details.
 """
 
+def test_root(test_client):
+    assert 'Complexity' in test_client.get('/').data
